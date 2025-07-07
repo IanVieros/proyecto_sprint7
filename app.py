460c04d262b58f308e22f1e9bedb5c3709efea3b
IanVieros <144949417+IanVieros@users.noreply.github.com>
2025-07-07 17:59:56 -0600

    update app.py

--- app.py
+++ app.py
@@ -4,21 +4,32 @@ import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header("prueba encabezado")
+st.header("Análisis Exploratorio de Vehiculos en Venta Dentro de E.E.U.U.")
 
-hist_button = st.button('Construir histograma')  # crear un botón
+hist_button = st.button("Mostrar histograma") 
 
 if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write("Este histograma muestra la distribución del kilometraje (`odometer`) de los vehículos en venta.")
     
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data,
+                       x="odometer",
+                       nbins=30,
+                       title="Distribución del kilometraje (Odometer)",
+                       labels= {"odometer": "kilometraje"},
+                       color_discrete_sequence=["#636EFA"])
     
     st.plotly_chart(fig, use_container_width=True)
 
-scattered_button = st.button("Construir cuadro de dipersión")
+scattered_button = st.button("Mostrar Diagrama de Dipersión")
 if scattered_button:
-    st.write("se crea cuadro de disperción... miaumiau")
+    st.write("Este gráfico muestra la relación entre el kilometraje y el precio de los vehículos.")
 
-    fig_sc = px.scatter(car_data, x="odometer", y="price")
+    fig_sc = px.scatter(car_data,
+                        x="odometer",
+                        y="price",
+                        title="Relación entre Kilometraje y Precio",
+                        labels={"odometer": "Kilometraje", "price": "Precio (USD"},
+                        opacity= 0.6,
+                        color_discrete_sequence=["#EF553B"])
 
     st.plotly_chart(fig_sc, use_container_width=True)
